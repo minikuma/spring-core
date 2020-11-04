7c2b3bc1925a43e89f6d9df8275be1ca1453997e
minikuma <minikuma@naver.com>
2020-11-04 14:17:47 +0900

    ver.0.9.0 bean cycle

---
+++ src/test/java/me/minikuma/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,34 @@
+package me.minikuma.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wminikuma@gmail.com on 2020/11/03
+ * Blog : https://minikuma-laboratory.tistory.com/
+ * Github : http://github.com/minikuma
+ */
+public class BeanLifeCycleTest {
+
+    @Test
+    public void bean_life_cycle_test() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient networkClient = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+
+}
